3d977d44e33de078eab6c9c19eef16163b64b091
stenbumling <marcusrosin@outlook.com>
2023-10-02 15:24:26 +0200

    style: add global variables for colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <script>
 	import '/src/lib/styles/app.css';
+	import '/src/lib/styles/vars.css';
 </script>
 
 <h1>License manager</h1>
 <h2>Subheader</h2>
 <h3>Button text</h3>
 <p>Some regular body text</p>
+
+<style>
+	h3 {
+		color: var(--alert);
+	}
+</style>
