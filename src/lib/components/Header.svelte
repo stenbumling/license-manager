ecf708e6e109e78e64ecad908b1c540db46583fa
stenbumling <marcusrosin@outlook.com>
2023-10-02 15:52:16 +0200

    feat: add header and Nexer logo

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,24 @@
+<header>
+	<div class="header-container">
+		<img src="src/lib/images/nexer-logo-white.png" alt="Nexer Logo" />
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+    justify-content: center;
+		padding: 1.4rem 3rem;
+		background-color: black;
+	}
+
+	img {
+		height: 2rem;
+	}
+
+	.header-container {
+    width: 100%;
+		max-width: 120rem;
+		display: flex;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script>
+	import Header from '$lib/components/Header.svelte';
 	import '/src/lib/styles/app.css';
 	import '/src/lib/styles/vars.css';
 </script>
 
+<Header />
+
 <h1>License manager</h1>
 <h2>Subheader</h2>
 <h3>Button text</h3>
-<p>Some regular body text</p>
+<p>Some regular body text <br /> and some more on a new line</p>
 
 <style>
 	h3 {
