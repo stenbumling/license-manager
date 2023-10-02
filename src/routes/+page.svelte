2eee20fa74015a705be4c63c9f499dde10421c1d
stenbumling <marcusrosin@outlook.com>
2023-10-02 15:02:11 +0200

    style: add custom styling for different types of text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Licenshanterare</h1>
+<script>
+	import '/src/lib/styles/app.css';
+</script>
+
+<h1>License manager</h1>
+<h2>Subheader</h2>
+<h3>Button text</h3>
+<p>Some regular body text</p>
