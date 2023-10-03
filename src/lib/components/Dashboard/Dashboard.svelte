c97146d6bd9cc8221e4cd91fa278ed46aca1059e
stenbumling <marcusrosin@outlook.com>
2023-10-03 15:40:22 +0200

    feat: add search bar component

--- src/lib/components/Dashboard/Dashboard.svelte
+++ src/lib/components/Dashboard/Dashboard.svelte
@@ -1,10 +1,12 @@
 <script>
-	import FilterButton from '$lib/components/dashboard/FilterButton.svelte';
 	import ButtonLarge from '$lib/components/ButtonLarge.svelte';
+	import FilterButton from '$lib/components/dashboard/FilterButton.svelte';
+	import SearchBar from '$lib/components/dashboard/SearchBar.svelte';
 </script>
 
 <div class="dashboard">
 	<h1>License <br /> manager</h1>
+	<SearchBar />
 	<h2>Filter</h2>
 	<FilterButton />
 	<FilterButton />
@@ -21,7 +23,7 @@
 		padding: 2rem;
 		border: 1px solid black;
 		width: 20rem;
-		height: 37rem;
+		height: 42rem;
 		margin-right: 3rem;
 		margin-top: 3.6rem;
 	}
---
+++ src/lib/components/Dashboard/SearchBar.svelte
@@ -0,0 +1,23 @@
+<button class="button-container">
+	<h3>Search</h3>
+</button>
+
+<style>
+	.button-container {
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+		background-color: white;
+		color: rgb(183, 181, 181);
+		border: 1px solid black;
+		height: 2.6rem;
+		width: 100%;
+		margin-bottom: 1.6rem;
+		margin-top: 0.6rem;
+		padding: 0.5rem 1.3rem;
+	}
+
+	h3 {
+		margin-top: 1px;
+	}
+</style>
