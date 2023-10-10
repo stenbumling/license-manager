eb081d21ff51dfd9aaee90cffe954621e9ee3a28
stenbumling <marcusrosin@outlook.com>
2023-10-10 15:36:34 +0200

    deps: add tailwind css & flowbite-svelte
    Mainly for the input fields, possibly for toasts and alert banners

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+
+  plugins: [require('flowbite/plugin')],
+
+  theme: {
+    extend: {}
+  },
+
+  plugins: []
+};
+
+module.exports = config;
