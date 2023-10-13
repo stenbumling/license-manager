6552fce9c9a5fb3553dbc21afe7064c8d2644d46
stenbumling <marcusrosin@outlook.com>
2023-10-13 13:42:03 +0200

    feat: add a search bar to service modal

--- src/lib/components/ServiceModal.svelte
+++ src/lib/components/ServiceModal.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { showServiceModal } from '$lib/stores/modal.ts';
 	import CloseLarge from 'carbon-icons-svelte/lib/CloseLarge.svelte';
+	import Search from 'carbon-icons-svelte/lib/Search.svelte';
 	import { fade } from 'svelte/transition';
 
 	function handleClick() {
@@ -10,9 +11,20 @@
 
 <div class="modal-container" transition:fade={{ duration: 120 }}>
 	<dialog open class="modal-window">
-		<a href="/" class="back-link" on:click|preventDefault={handleClick}>
-			<CloseLarge size={24} aria-label="CloseLarge" />
-		</a>
+		<div class="modal-header">
+			<h1 class="modal-title">Service list</h1>
+			<a href="/" class="back-link" on:click|preventDefault={handleClick}>
+				<CloseLarge size={24} aria-label="CloseLarge" />
+			</a>
+		</div>
+		<div class="search-container">
+			<div class="search-icon">
+				<Search size={20} fill="black" aria-label="Search" />
+			</div>
+			<input class="search-field" type="text" placeholder="Search" />
+		</div>
+		<div>List</div>
+		<div>Confirmation buttons</div>
 	</dialog>
 </div>
 
@@ -31,13 +43,25 @@
 	.modal-window {
 		border: none;
 		display: flex;
+		flex-direction: column;
 		align-self: center;
-		justify-content: flex-end;
 		width: 40vw;
 		max-width: 100rem;
 		height: 60vh;
 		background-color: white;
 		padding: 2rem 4rem;
+	}
+
+	.modal-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin: 0 0 2rem 0;
+	}
+
+	.modal-title {
+		font-size: 2rem;
+		margin: 0;
 	}
 
 	.back-link {
@@ -46,7 +70,6 @@
 		color: black;
 		transition: color 0.25s ease;
 		display: flex;
-		align-self: flex-start;
 		padding: 0.2rem;
 		border-radius: 6px;
 
@@ -59,5 +82,39 @@
 			top: 1px;
 			left: 1px;
 		}
+	}
+
+	.search-container {
+		margin-bottom: 2rem;
+	}
+
+	.search-icon {
+		position: absolute;
+		top: 117px;
+		left: 76px;
+	}
+
+	.search-field {
+		font-family: 'FK Grotesk Regular', Arial, Helvetica, sans-serif;
+		border: none;
+		width: 100%;
+		height: 3rem;
+		background-color: transparent;
+		border-bottom: 1px solid var(--text-placeholder);
+		box-sizing: border-box;
+		padding-left: 50px;
+	}
+
+	input:hover {
+		border: 1px dashed black;
+		padding-left: 49px;
+		padding-bottom: 2px;
+	}
+
+	input:focus {
+		border: 2px solid var(--light-purple);
+		outline: none;
+		padding-left: 48px;
+		padding-bottom: 2px;
 	}
 </style>
