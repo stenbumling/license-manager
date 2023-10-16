abccd902151a781efd11c165ea33b2b9a4bc6fdf
stenbumling <marcusrosin@outlook.com>
2023-10-16 11:11:45 +0200

    style: moved delete button to the left side

--- src/lib/components/license/application-admin/ApplicationItem.svelte
+++ src/lib/components/license/application-admin/ApplicationItem.svelte
@@ -4,31 +4,38 @@
 </script>
 
 <div class="application-item">
-	<p>{label}</p>
 	<div class="icon">
 		<TrashCan size={24} fill="red" />
 	</div>
+	<p>{label}</p>
 </div>
 
 <style>
 	.application-item {
 		width: 100%;
-		height: 3rem;
+		height: 4rem;
 		/* background-color: red; */
 		display: flex;
-		justify-content: space-between;
 		align-items: center;
-		margin: 0.2rem 0;
+		margin: 0 0 0.2rem 0;
 		border-bottom: 1px solid #E6E6E6;
 	}
 
 	.icon {
-		width: 2.2rem;
-		height: 2.2rem;
-		/* background-color: red; */
+		width: 2rem;
+		height: 2rem;
+		/* background-color: #dd4848; */
 		border-radius: 8px;
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		margin-right: 1rem;
+		cursor: pointer;
+		position: relative;
+		top: -1px;
+
+		&:hover {
+			background-color: #ffefef;
+		}
 	}
 </style>
