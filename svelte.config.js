7949e2fee14c014936364e42c3ab029164a52a39
stenbumling <marcusrosin@outlook.com>
2023-10-17 10:37:28 +0200

    config: add proxy to backend

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,17 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		vite: {
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://81.232.97.112',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          },
+        },
+      },
+    },
 	},
 };
 
