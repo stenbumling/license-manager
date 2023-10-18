2d92a90748ebdded405c34d37f51ffe7f16c65dd
stenbumling <marcusrosin@outlook.com>
2023-10-18 08:55:45 +0200

    fix: move proxy configuration to appropriate place

--- svelte.config.js
+++ svelte.config.js
@@ -12,18 +12,18 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		vite: {
-      server: {
-        proxy: {
-          '/api': {
-            target: 'http://81.232.97.112:1433',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ''),
-          },
+	},
+  vite: {
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
     },
-	},
+  },
 };
 
 export default config;
