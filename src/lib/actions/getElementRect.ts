9dad2fc70c46a9bbeda784560fa6b460cbc228b4
stenbumling <marcusrosin@outlook.com>
2023-11-06 17:05:21 +0100

    feat: listen to window resizing and adjust context menu position

--- src/lib/actions/getElementRect.ts
+++ src/lib/actions/getElementRect.ts
@@ -1,4 +1,5 @@
 export function getElementRect(node: Element, callback: (arg0: DOMRect) => void) {
+	// Observes and returns the bounding rectangle of the given node when it changes
 	const observer = new ResizeObserver((entries) => {
 		if (entries[0].target === node) {
 			const rect = entries[0].target.getBoundingClientRect();
@@ -13,12 +14,33 @@ export function getElementRect(node: Element, callback: (arg0: DOMRect) => void)
 	});
 
 	observer.observe(node);
-	window.addEventListener('resize', () => callback(node.getBoundingClientRect()));
+
+	// Listen for window resize events and debounces them if too frequent
+	let debounceTimeout: number | undefined;
+
+	function debounce(
+		func: (...args: unknown[]) => void,
+		delay: number,
+	): (...args: unknown[]) => void {
+		let debounceTimeout: number | undefined;
+
+		return function (this: unknown, ...args: unknown[]) {
+			clearTimeout(debounceTimeout);
+			debounceTimeout = window.setTimeout(() => func(...args), delay);
+		};
+	}
+	const debouncedResize = debounce(() => {
+		callback(node.getBoundingClientRect());
+	}, 100);
+
+	window.addEventListener('resize', debouncedResize);
 
 	return {
 		destroy() {
 			observer.unobserve(node);
 			observer.disconnect();
+			window.removeEventListener('resize', debouncedResize);
+			if (debounceTimeout) clearTimeout(debounceTimeout);
 		},
 	};
 }
