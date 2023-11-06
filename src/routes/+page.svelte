f0c2a2df74e1c9596d732f0698494965508c96bf
stenbumling <marcusrosin@outlook.com>
2023-11-06 20:39:12 +0100

    fix: remove goback at mount

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Dashboard from '$lib/components/dashboard/Dashboard.svelte';
 	import LicenseModal from '$lib/components/license/LicenseModal.svelte';
 	import TableContainer from '$lib/components/table/TableContainer.svelte';
 	import { showLicenseModal } from '$lib/stores/modal-state';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		goto('/');
-	});
 
 	$: if ($page.url.search.startsWith('?modal')) {
 		showLicenseModal.set(true);
