336b7ad2dd48892ca7db4ece0fca2694b84b500e
stenbumling <marcusrosin@outlook.com>
2023-11-17 14:26:49 +0100

    deps: install azure adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,18 +10,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(
-			{
-				// default options are shown
-				pages: 'build',
-				assets: 'build',
-				fallback: null,
-			},
-			{
-				// default options are shown
-				amp: false,
-			},
-		),
+		adapter: azure(),
 	},
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
-			'/api': 'https://nexer-orebro-license-server.azurewebsites.net',
-			// '/api': 'http://localhost:3000',
+			'/api': 'https://nexer-orebro-license-app.azurewebsites.net',
 		},
 	},
 });
