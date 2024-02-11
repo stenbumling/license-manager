7db375a943d68467991652b862346a62105fea68
stenbumling <marcusrosin@outlook.com>
2024-02-11 18:29:49 +0100

    config: change server url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
-			'/api': 'http://localhost:3000',
+			'/api': "https://nexer-orebro-license-app-server.azurewebsites.net",
 		},
 	},
 });
