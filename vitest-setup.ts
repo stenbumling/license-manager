9667f1775da6fa1c05fbcee642a3d43a8f3e2962
stenbumling <marcusrosin@outlook.com>
2024-03-06 10:06:26 +0100

    test: install and setup test frameworks

---
+++ vitest-setup.ts
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom/vitest';
+import '@testing-library/svelte/vitest';
+import { afterEach } from 'vitest';
+
+afterEach(() => {
+	// TODO: Post-test cleanup (if necessary)
+});
