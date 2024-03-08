9a667ad8b07d38fe1a63838453a3eeab5f8fcdca
stenbumling <marcusrosin@outlook.com>
2024-03-08 14:43:07 +0100

    feat: add msw for API mocking

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,7 +1,8 @@
 import '@testing-library/jest-dom/vitest';
 import '@testing-library/svelte/vitest';
-import { afterEach } from 'vitest';
+import { afterAll, afterEach, beforeAll } from 'vitest';
+import { server } from './src/mocks/node';
 
-afterEach(() => {
-	// TODO: Post-test cleanup (if necessary)
-});
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
