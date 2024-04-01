19b67be30953321784629d2e04b4c2b22fada0de
stenbumling <marcusrosin@outlook.com>
2024-04-01 12:35:09 +0200

    feat: improve how database connections are handled

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,26 +1,47 @@
-import { Sequelize } from 'sequelize';
-
-export const sequelize = createSequelizeInstance();
+import { ConnectionError, ConnectionTimedOutError, Sequelize, TimeoutError } from 'sequelize';
+import type { ConnectionConfiguration } from 'tedious';
 
 function createSequelizeInstance() {
 	const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
-
 	if (!DB_HOST || !DB_NAME || !DB_USER || !DB_PASSWORD) {
 		throw new Error(
 			'Missing database configuration! Make sure you have set the following environment variables: DB_HOST, DB_NAME, DB_USER and DB_PASSWORD. Check the .env.locals.example file for reference.',
 		);
-		// TODO: Handle database connection errors in a more graceful and user-friendly way
-	}
-	return new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-		host: DB_HOST,
-		dialect: 'mssql',
-		dialectOptions: {
-			options: {
-				dateFirst: 1,
+		// TODO: Handle this error in a more graceful and user-friendly way
+	} else {
+		return new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+			host: DB_HOST,
+			dialect: 'mssql',
+			dialectOptions: {
+				options: getTediousConnectionOptions(),
 			},
-		},
-		logging: (query) => console.log(new Date(), query),
-	});
+			logging: (query) => console.log(new Date(), query),
+			retry: getRetryOptions(),
+		});
+	}
+}
+
+function getTediousConnectionOptions() {
+	const options: ConnectionConfiguration['options'] = {
+		connectTimeout: 60000,
+		requestTimeout: 60000,
+		datefirst: 1,
+	};
+	return options;
+}
+
+function getRetryOptions() {
+	return {
+		max: 3,
+		timeout: 60000,
+		match: [
+			ConnectionError,
+			ConnectionTimedOutError,
+			TimeoutError,
+			'SequelizeConnectionError',
+			'SQLITE_BUSY',
+		],
+	};
 }
 
 export async function initDb() {
@@ -29,7 +50,7 @@ export async function initDb() {
 		await syncDbModels();
 	} catch (error) {
 		console.error('Unable to connect to the database: ', error);
-		// TODO: Handle database connection errors in a more graceful and user-friendly way
+		// TODO: Handle this in a more graceful and user-friendly way
 	}
 }
 
@@ -43,3 +64,5 @@ async function syncDbModels() {
 	await sequelize.sync();
 	console.log('Database schemas synchronized with the models');
 }
+
+export const sequelize = createSequelizeInstance();
