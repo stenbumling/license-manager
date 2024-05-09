a0d8959df1f43cf6e7726885125da12b9e93db74
stenbumling <marcusrosin@outlook.com>
2024-05-09 10:29:28 +0200

    style: color changes to flagged badges

--- src/lib/components/license/fields/UserBadge.svelte
+++ src/lib/components/license/fields/UserBadge.svelte
@@ -3,6 +3,7 @@
 	import { currentLicense } from '$lib/stores/resources/license-store';
 	import type { User } from '$lib/stores/resources/user-store';
 	import CloseFilled from 'carbon-icons-svelte/lib/CloseFilled.svelte';
+	import WarningFilled from 'carbon-icons-svelte/lib/WarningFilled.svelte';
 
 	export let user: User;
 
@@ -11,29 +12,24 @@
 	}
 </script>
 
-<div
-	class="badge"
-	style:background-color={user.active ? `var(--deep-purple)` : '#e0e0e0'}
-	style:color={user.active ? 'white' : '#888888'}
->
-	{#if user.active === true}
-		<div class="badge-text-container">
-			<span class="badge-text">{user.name}</span>
-		</div>
-	{:else}
+<div class="badge" style:background-color={user.active ? `var(--deep-purple)` : '#d32d2d'}>
+	{#if user.active !== true}
 		<div
-			class="badge-text-container"
+			class="badge-conflict-icon"
 			use:tooltip={{
 				content:
 					'This user does no longer exist in your Azure AD group. You can unassign the user, but you cannot reassign them. If this user is still active, please contact your administrator.',
 				options: { delay: [500, 0] },
 			}}
 		>
-			<span class="badge-text">{user.name}</span>
+			<WarningFilled size={16} />
 		</div>
 	{/if}
+	<div class="badge-text-container">
+		<span class="badge-text">{user.name}</span>
+	</div>
 	<button class="badge-delete-button" on:click={() => handleRemoveUser(user)}>
-		<CloseFilled fill={user.active ? 'white' : '#888888'} size={16} />
+		<CloseFilled fill="white" size={16} />
 	</button>
 </div>
 
@@ -46,6 +42,7 @@
 		padding: 0 0.6rem;
 		margin: 0.2rem 0.4rem 0.2rem 0;
 		height: 36px;
+		color: white;
 	}
 
 	.badge-text-container {
@@ -65,11 +62,18 @@
 		user-select: none;
 	}
 
+	.badge-conflict-icon {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		margin-right: 3px;
+	}
+
 	.badge-delete-button {
 		box-sizing: border-box;
 		cursor: pointer;
 		display: flex;
-		height: 50%;
+		height: 100%;
 		align-items: center;
 	}
 
@@ -78,7 +82,11 @@
 		opacity: 0.5;
 	}
 
-	.badge-delete-button:focus-within {
+	.badge-delete-button:active > :global(svg) {
+		transform: scale(0.9);
+	}
+
+	.badge-delete-button:focus-visible {
 		box-shadow: 0 0 0 2px white;
 		border-radius: 4px;
 	}
